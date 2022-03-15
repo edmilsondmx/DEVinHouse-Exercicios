ecae199d73f74e1063f4343c125ced9f27ec7f9d
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-15 14:18:25 -0300

    criado uma função que coloca o 0 antes da hora e/ou dos minutos quando menor que 10

--- Exercicios09-Semana03/script.js
+++ Exercicios09-Semana03/script.js
@@ -4,6 +4,17 @@ var hoje = new Date
 var hora = hoje.getHours()
 var minuto = hoje.getMinutes()
 
+function checkTime(i) {
+    if (i < 10){
+        i = "0" +  i
+    }
+    return i
+}
+
+hora = checkTime(hora)
+minuto = checkTime(minuto)
+
+
 window.alert(hora + ":" + minuto)
 
 res.innerHTML = `${hora}:${minuto}`
