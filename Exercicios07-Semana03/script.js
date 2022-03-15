24320c7002ba571467db021f853d110eb2af582a
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-15 13:15:10 -0300

    criação id com resposta no documento

--- Exercicios07-Semana03/script.js
+++ Exercicios07-Semana03/script.js
@@ -1,7 +1,12 @@
+var res = document.getElementById('res')
+
 var anoNasc = prompt("Qual ano do seu nascimento?")
 var anoAtual = prompt("Qual o ano atual?")
 
 var idadeSjf = anoAtual - anoNasc
 var idadeSnf = (anoAtual - 1) - anoNasc
 
+
 window.alert("Sua idade é " + idadeSjf + " ou " + idadeSnf + "!")
+
+res.innerHTML = `Sua idade é ${idadeSjf} ou ${idadeSnf}!`
