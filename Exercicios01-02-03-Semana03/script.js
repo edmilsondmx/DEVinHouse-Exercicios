59a40dd66ca9676b1c54308f3e1ac2bd886596ce
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-15 12:16:42 -0300

    criação do exercicio 04

---
+++ Exercicios01-02-03-Semana03/script.js
@@ -0,0 +1,4 @@
+//window.alert("Olá DEV, Bem vindo ao meu site, fique a vontade!")
+//window.confirm("Tem certeza que deseja abrir esse site?")
+//var user = window.prompt("Qual o seu nome?")
+//window.alert("Olá "+ user + "!")
