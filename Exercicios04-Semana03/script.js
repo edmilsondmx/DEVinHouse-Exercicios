9076e7ef69a3a2c7958e2c3ca77cc1e7a0e16579
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-15 12:32:59 -0300

    correção script

--- Exercicios04-Semana03/script.js
+++ Exercicios04-Semana03/script.js
@@ -1,4 +1,4 @@
 var nome = String(window.prompt("Qual seu nome?"));
 var idade = Number(window.prompt("Qual sua idade?"));
 var esporte = Boolean(window.confirm("Gosta de praticar algum esporte?"));
-console.log("Nome: " + nome + ", Idade: " + idade + ",Esporte: " + esporte);
+console.log("Nome: " + nome + ", Idade: " + idade + ", Esporte: " + esporte);
