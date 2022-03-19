4889d1858d02116aaefac7992337ea1215d5a343
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-18 21:18:47 -0300

    alteração exercicio 10

--- Exercicios-Semana03/Exercicios10-Semana03/script.js
+++ Exercicios-Semana03/Exercicios10-Semana03/script.js
@@ -2,10 +2,14 @@ var vInicial = Number(prompt("Qual o valor inicial?"));
 var vRaiz = Number(prompt("Qual o valor raiz?"));
 var res = document.getElementById('res')
 
+var result = [];
+
 for (var i = 0; i < 10; i++) {
     res.innerHTML += vInicial + " "
+    result.push(vInicial)
     vInicial += vRaiz
     
 };
+alert(result.join(" ") )
 
 
