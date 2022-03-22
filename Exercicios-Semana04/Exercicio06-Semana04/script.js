e6c705dbfe1ed68408e5abf8e9a64b674a54b417
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-22 17:23:55 -0300

    [M1S04] - Ex 6 - Execute o código enquanto o valor seja verdadeiro

--- Exercicios-Semana04/Exercicio06-Semana04/script.js
+++ Exercicios-Semana04/Exercicio06-Semana04/script.js
@@ -1,16 +1,11 @@
-// Jovens - Indivíduos de até 15 anos;
-// Adultos - Indivíduos com idade entre 16 até 64 anos;
-// Idosos - Indivíduos de 65 anos em diante.
+var impares = 0;
+var cont = 0;
 
-var idade = prompt("Qual a sua idade?");
-
-if(idade <= 15){
-    alert("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-} else if(idade < 65){
-    alert("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-} else{
-    alert("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Idosa!");
+while (impares < 10) {
+    cont++;
+    if(cont % 2 != 0){
+        impares++
+    };
+    console.log(`Contador: ${cont}`);
+    console.log(`Impares: ${impares}`);
 };
