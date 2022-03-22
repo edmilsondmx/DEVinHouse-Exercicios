2b753781696d1c28db12b6ab272ff3c84e636d0c
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-22 16:34:01 -0300

    [M1S04] - Ex 2 - Realize as operações matemáticas básicas

--- Exercicios-Semana04/Exercicio02-Semana04/script.js
+++ Exercicios-Semana04/Exercicio02-Semana04/script.js
@@ -1,16 +1,45 @@
-// Jovens - Indivíduos de até 15 anos;
-// Adultos - Indivíduos com idade entre 16 até 64 anos;
-// Idosos - Indivíduos de 65 anos em diante.
-
-var idade = prompt("Qual a sua idade?");
-
-if(idade <= 15){
-    alert("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-} else if(idade < 65){
-    alert("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-} else{
-    alert("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Idosa!");
+/* function calcular(num1, num2, operador){
+    switch(operador) {
+        case "+":
+            console.log(`A soma de ${num1} e ${num2} é: ${num1 + num2}.`);
+            break;
+        case "-":
+            console.log(`A subtração de ${num1} e ${num2} é: ${num1 - num2}.`);
+            break;
+        case "*":
+            console.log(`A multiplicação de ${num1} e ${num2} é: ${num1 * num2}.`);
+            break;
+        case "/":
+            console.log(`A divisão de ${num1} e ${num2} é: ${num1 / num2}.`);
+            break;
+        default:
+            console.log("Operador não disponível!");
+    }
 };
+
+calcular(2, 8, "+");
+calcular(20, 5, "-");
+calcular(7, 9, "*");
+calcular(15, 3, "/");
+calcular(11, 3, "%"); */
+
+function calcular(num1, num2, operador){
+    switch(operador) {
+        case "+":
+            alert(`A soma de ${num1} e ${num2} é: ${num1 + num2}.`);
+            break;
+        case "-":
+            alert(`A subtração de ${num1} e ${num2} é: ${num1 - num2}.`);
+            break;
+        case "*":
+            alert(`A multiplicação de ${num1} e ${num2} é: ${num1 * num2}.`);
+            break;
+        case "/":
+            alert(`A divisão de ${num1} e ${num2} é: ${num1 / num2}.`);
+            break;
+        default:
+            alert("Operador não disponível!");
+    }
+};
+
+calcular(Number(prompt("Qual o primeiro número?")), Number(prompt("Qual o segundo número?")), prompt("Qual a operação: ' + ' ' - ' ' * ' ' / '"));
