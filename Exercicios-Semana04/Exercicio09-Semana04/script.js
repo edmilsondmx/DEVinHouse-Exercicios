3e255b8235ee1193f2bc7833ee65d6738784dc29
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-22 18:23:31 -0300

    [M1S04] - Ex 9 - Verificar se array contem item

--- Exercicios-Semana04/Exercicio09-Semana04/script.js
+++ Exercicios-Semana04/Exercicio09-Semana04/script.js
@@ -1,16 +1,14 @@
-// Jovens - Indivíduos de até 15 anos;
-// Adultos - Indivíduos com idade entre 16 até 64 anos;
-// Idosos - Indivíduos de 65 anos em diante.
+const cidades = [
+    { nome: 'Patos de Minas', populacao: 153585 },
+    { nome: 'Lages', populacao: 157349 },
+    { nome: 'Ibiúna', populacao: 79479 },
+    { nome: 'Maringá', populacao: 403063 },
+    { nome: 'Curitiba', populacao: 1963726 },
+    { nome: 'Florianópolis', populacao: 508826 },
+    { nome: 'Pato Branco', populacao: 84779 },
+  ];
 
-var idade = prompt("Qual a sua idade?");
+ var res = cidades.some(item => item.nome.toUpperCase() || item.nome.toLowerCase() === "florianopolis");
 
-if(idade <= 15){
-    alert("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-} else if(idade < 65){
-    alert("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-} else{
-    alert("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-};
+ console.log(res)
+
