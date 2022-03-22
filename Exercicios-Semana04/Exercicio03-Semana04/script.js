82ff0de99c0896152b8582b729904649fc7962de
Edmilson Gomes <edmilsondmx@gmail.com>
2022-03-22 16:45:38 -0300

    [M1S04] - Ex 3 - Numero é par ou  ímpar

--- Exercicios-Semana04/Exercicio03-Semana04/script.js
+++ Exercicios-Semana04/Exercicio03-Semana04/script.js
@@ -1,16 +1,4 @@
-// Jovens - Indivíduos de até 15 anos;
-// Adultos - Indivíduos com idade entre 16 até 64 anos;
-// Idosos - Indivíduos de 65 anos em diante.
+var num = prompt("Digite um número:");
 
-var idade = prompt("Qual a sua idade?");
-
-if(idade <= 15){
-    alert("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Jovem!");
-} else if(idade < 65){
-    alert("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Adulta!");
-} else{
-    alert("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-    console.log("Você tem " + idade + " anos, então é uma pessoa Idosa!");
-};
+(num % 2 == 0)?alert(`O Número ${num} é Par!`):alert(`O Número ${num} é Ímpar!`);
+(num % 2 == 0)?console.log(`O Número ${num} é Par!`):console.log(`O Número ${num} é Ímpar!`);
