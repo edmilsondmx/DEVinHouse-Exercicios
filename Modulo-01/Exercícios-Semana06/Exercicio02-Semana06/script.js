cbb7e35567b091eba4a917658f73ea27ac88e599
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-06 12:31:55 -0300

    [M1S06] - Ex 2 - Manipulando DOM

--- Modulo-01/Exercícios-Semana06/Exercicio02-Semana06/script.js
+++ Modulo-01/Exercícios-Semana06/Exercicio02-Semana06/script.js
@@ -1,7 +1,34 @@
 const botao = document.getElementById('btn');
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+const operador = document.getElementById('operador');
+const res = document.getElementById('res')
 
+const paragrafo = document.createElement('p');
 
-botao.addEventListener('click', alteraTitulo);
-function alteraTitulo(){
-    document.getElementById('title').textContent = 'Título da Página Alterado!'
+botao.addEventListener('click', calcular);
+function calcular(){
+    if(num1.value.length === 0 || num2.value.length === 0 || operador.value == ""){
+        alert('ERRO: Não foi possivel fazer a operação!');
+    } else{
+        switch (operador.value) {
+            case 'soma':
+                res.innerHTML += `O Resultado é: ${Number(num1.value) + Number(num2.value)}`;
+                break;
+            case 'subtracao':
+                res.innerHTML += `O Resultado é: ${Number(num1.value) - Number(num2.value)}`;
+                break;
+            case 'multiplicacao':
+                res.innerHTML += `O Resultado é: ${Number(num1.value) * Number(num2.value)}`;
+                break;
+            case 'divisao':
+                res.innerHTML += `O Resultado é: ${Number(num1.value) / Number(num2.value)}`;
+                break;
+        
+            default:
+                break;
+        };
+    };
+    
+    res.appendChild(paragrafo);
 };
