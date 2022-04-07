2b8e887529bcb95787788ba07ff83b009d204339
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-07 14:57:46 -0300

    [M1S06] - Ex 10 - Manipulação de DOM

--- Modulo-01/Exercícios-Semana06/Exercicio10-Semana06/script.js
+++ Modulo-01/Exercícios-Semana06/Exercicio10-Semana06/script.js
@@ -1,34 +1,14 @@
 const botao = document.getElementById('btn');
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
-const operador = document.getElementById('operador');
-const res = document.getElementById('res')
 
-const paragrafo = document.createElement('p');
+botao.addEventListener('click', inverter);
 
-botao.addEventListener('click', calcular);
-function calcular(){
-    if(num1.value.length === 0 || num2.value.length === 0 || operador.value == ""){
-        alert('ERRO: Não foi possivel fazer a operação!');
-    } else{
-        switch (operador.value) {
-            case 'soma':
-                paragrafo.innerHTML = `O Resultado é: ${Number(num1.value) + Number(num2.value)}`;
-                break;
-            case 'subtracao':
-                paragrafo.innerHTML = `O Resultado é: ${Number(num1.value) - Number(num2.value)}`;
-                break;
-            case 'multiplicacao':
-                paragrafo.innerHTML = `O Resultado é: ${Number(num1.value) * Number(num2.value)}`;
-                break;
-            case 'divisao':
-                paragrafo.innerHTML = `O Resultado é: ${Number(num1.value) / Number(num2.value)}`;
-                break;
-        
-            default:
-                break;
-        };
-    };
-    
-    res.appendChild(paragrafo);
+const CLASS_RED = 'vermelho';
+const CLASS_BLUE = 'azul'
+
+function inverter() {
+    const listaItens = document.querySelectorAll('li')
+
+    listaItens.forEach(item => {
+        item.className === CLASS_BLUE?item.className = CLASS_RED:item.className = CLASS_BLUE;
+    });
 };
