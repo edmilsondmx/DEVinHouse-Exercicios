1134a931d67e476f037d2e4f02663a799ed86d2a
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-26 22:48:24 -0300

    [M1S09] - Ex 3 - Objetos, Classes e Propriedades

---
+++ Modulo-01/Exercicios-Semana09/Exercicio03-Semana09/exercicio03.ts
@@ -0,0 +1,12 @@
+let aluno:{
+    nome: string,
+    matricula: number,
+    ativo: boolean,
+    cadeiras: string[] 
+};
+aluno.nome = 'Edmilson';
+aluno.matricula = 800188;
+aluno.ativo = true;
+aluno.cadeiras = ["JavaScript", "Angular", "C#", ".Net Core", "Scrum"];
+
+console.log(aluno)
