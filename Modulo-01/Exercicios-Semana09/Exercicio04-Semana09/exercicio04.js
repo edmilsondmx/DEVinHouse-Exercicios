51feef22e9b2cd9f87f8f408af2516dbe909e5ca
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-26 22:59:25 -0300

    [M1S09] - Ex 4 - Objetos, Classes e Propriedades

---
+++ Modulo-01/Exercicios-Semana09/Exercicio04-Semana09/exercicio04.js
@@ -0,0 +1,10 @@
+var Filme = /** @class */ (function () {
+    function Filme() {
+    }
+    return Filme;
+}());
+var homemAranha = new Filme();
+homemAranha.nome = "Homem-Aranha: Sem Volta para Casa";
+homemAranha.anoDeLancamento = 2021;
+homemAranha.diretor = "Stan Lee";
+console.log(homemAranha);
---
+++ Modulo-01/Exercicios-Semana09/Exercicio04-Semana09/exercicio04.ts
@@ -0,0 +1,12 @@
+class Filme{
+    nome:string;
+    anoDeLancamento:number;
+    diretor:string;
+}
+
+const homemAranha = new Filme();
+homemAranha.nome = "Homem-Aranha: Sem Volta para Casa";
+homemAranha.anoDeLancamento = 2021;
+homemAranha.diretor = "Stan Lee";
+
+console.log(homemAranha)
