c02a99f187be9930a9b7d9ad5ed2e0bd9353b243
Edmilson Gomes <edmilsondmx@gmail.com>
2022-04-29 21:41:11 -0300

    organização das pastas

--- Modulo-01/Exercícios-Semana06/Exercicio01-Semana06/script.js
+++
@@ -1,7 +0,0 @@
-const botao = document.getElementById('btn');
-
-
-botao.addEventListener('click', alteraTitulo);
-function alteraTitulo(){
-    document.getElementById('title').textContent = 'Título da Página Alterado!'
-};
