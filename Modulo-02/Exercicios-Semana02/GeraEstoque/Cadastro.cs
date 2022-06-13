699311e7c0dff1bebdb94724f0fea3deba86a55e
Edmilson Gomes <edmilsondmx@gmail.com>
2022-06-13 19:03:10 -0300

    [M2S02] - Ex 9 - Projeto Aplicado 5

--- Modulo-02/Exercicios-Semana02/GeraEstoque/Cadastro.cs
+++ Modulo-02/Exercicios-Semana02/GeraEstoque/Cadastro.cs
@@ -17,6 +17,8 @@ public static class Cadastro
 
     public static void CadastrarProduto()
     {
+        var idProduto = Guid.NewGuid().ToString().Substring(0, 8);
+
         Console.SetCursorPosition(2, 4);
         Console.Write("Nome do Produto: ");
         string nomeProduto = Console.ReadLine();
@@ -27,10 +29,36 @@ public static class Cadastro
 
         Console.SetCursorPosition(2, 6);
         Console.Write("Valor de Compra: ");
-        float valorCompra = float.Parse(Console.ReadLine());
+        double valorCompra = double.Parse(Console.ReadLine());
 
         Console.SetCursorPosition(2, 7);
         Console.Write("Valor de Venda: ");
-        float valorVenda = float.Parse(Console.ReadLine());
+        double valorVenda = double.Parse(Console.ReadLine());
+
+        ShowProduto(idProduto, nomeProduto, quantidadeEstoque, valorCompra, valorVenda);
+    }
+
+    public static void ShowProduto(string id, string? nome, ushort qtd, double vCompra, double vVenda)
+    {
+        Console.Clear();
+        Menu.Bordas();
+
+        Console.SetCursorPosition(2, 1);
+        System.Console.WriteLine("Produto Cadastrado com Sucesso!");
+        Console.SetCursorPosition(2, 2);
+        System.Console.WriteLine("-------------------------------");
+
+        Console.SetCursorPosition(2, 4);
+        System.Console.WriteLine($"ID: {id}");
+        Console.SetCursorPosition(2, 5);
+        System.Console.WriteLine($"Nome: Produto {nome}");
+        Console.SetCursorPosition(2, 6);
+        System.Console.WriteLine($"Qtd: {qtd}");
+        Console.SetCursorPosition(2, 7);
+        System.Console.WriteLine($"R$ Compra: R$ {vCompra}");
+        Console.SetCursorPosition(2, 8);
+        System.Console.WriteLine($"R$ Venda: R$ {vVenda}");
+
+
     }
 }
