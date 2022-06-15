da08d7b452986fd17aa8dec794750975d3fcf6e3
Edmilson Gomes <edmilsondmx@gmail.com>
2022-06-15 15:59:34 -0300

    [M2S03] Ex 4 - Projeto Aplicado v2

--- Modulo-02/Exercicios-Semana03/GeraEstoque/Screens/CriarProdutoScreen.cs
+++ Modulo-02/Exercicios-Semana03/GeraEstoque/Screens/CriarProdutoScreen.cs
@@ -1,6 +1,70 @@
 namespace GeraEstoque.Screens;
 
-public static class CriarProduto
+public static class CriarProdutoScreen
 {
+    public static void Iniciar()
+    {
+        Console.Clear();
+        MenuScreen.Bordas();
 
+        Console.SetCursorPosition(2, 1);
+        System.Console.WriteLine("Área de Cadastro");
+        Console.SetCursorPosition(2, 2);
+        System.Console.WriteLine("=================");
+
+        var idProduto = Guid.NewGuid().ToString().Substring(0, 8);
+
+        Console.SetCursorPosition(2, 4);
+        Console.Write("Nome do Produto: ");
+        string nomeProduto = Console.ReadLine();
+
+        Console.SetCursorPosition(2, 5);
+        Console.Write("Quantidade em estoque: ");
+        ushort quantidadeEstoque = ushort.Parse(Console.ReadLine());
+
+        Console.SetCursorPosition(2, 6);
+        Console.Write("Valor de Compra: ");
+        decimal valorCompra = decimal.Parse(Console.ReadLine());
+
+        Console.SetCursorPosition(2, 7);
+        Console.Write("Valor de Venda: ");
+        decimal valorVenda = decimal.Parse(Console.ReadLine());
+
+        ShowProduto(idProduto, nomeProduto, quantidadeEstoque, valorCompra, valorVenda);
+    }
+
+    public static void ShowProduto(string id, string? nome, ushort qtd, decimal vCompra, decimal vVenda)
+        {
+            Console.Clear();
+            MenuScreen.Bordas();
+
+            Console.SetCursorPosition(2, 1);
+            System.Console.WriteLine("Produto Cadastrado com Sucesso!");
+            Console.SetCursorPosition(2, 2);
+            System.Console.WriteLine("-------------------------------");
+
+            Console.SetCursorPosition(2, 4);
+            System.Console.WriteLine($"ID: {id}");
+            Console.SetCursorPosition(2, 5);
+            System.Console.WriteLine($"Nome: Produto {nome}");
+            Console.SetCursorPosition(2, 6);
+            System.Console.WriteLine($"Qtd: {qtd}");
+            Console.SetCursorPosition(2, 7);
+            System.Console.WriteLine($"R$ Compra:{vCompra.ToString("c")}");
+            Console.SetCursorPosition(2, 8);
+            System.Console.WriteLine($"R$ Venda:{vVenda.ToString("c")}");
+
+            Console.SetCursorPosition(2, 10);
+            System.Console.WriteLine("Pressione qualquer tecla");
+            Console.SetCursorPosition(2, 11);
+            System.Console.Write("para voltar ao menu principal!");
+            
+            if(Console.ReadKey().Key != ConsoleKey.F1)
+            {
+                MenuScreen.Iniciar();
+            }
+
+
+
+        }
 }
