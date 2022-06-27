b28ff41164dfb4729600527f206edaf3aed18e36
Edmilson Gomes <edmilsondmx@gmail.com>
2022-06-27 16:13:35 -0300

    [M2S04] Ex 9 Criar classe internal chamada CursoSelecionado

---
+++ Modulo-02/Exercicios-Semana04/FormulárioInscricao/CursoSelecionado.cs
@@ -0,0 +1,13 @@
+namespace FormularioInscricao;
+
+internal class CursoSelecionado
+{
+    private List<string> Cursos { get; set; }
+
+    public CursoSelecionado()
+    {
+        Cursos = new List<string>();
+
+    }
+
+}
