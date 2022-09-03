f6c7d7bee2504853b05096928448546b671a4ab2
Edmilson Gomes <edmilsondmx@gmail.com>
2022-09-03 15:34:53 -0300

    [M3S01] Ex 2 - Criando Banco de dados a partir das Models (Code First)

---
+++ Modulo-03/Exercicios-Semana01/DevIn-Lab-Rest-Escola/src/Escola.Infra/DataBase/Mappings/MateriaMap.cs
@@ -0,0 +1,25 @@
+using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Escola.Infra.DataBase.Mappings;
+
+public class MateriaMap : IEntityTypeConfiguration<Materia>
+{
+    public void Configure(EntityTypeBuilder<Materia> builder)
+    {
+        builder.ToTable("Materia");
+
+        builder.HasKey(m => m.Id);
+
+        builder.Property(m => m.Id)
+                .HasColumnName("ID")
+                .UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+        builder.Property(m => m.Nome)
+                .HasColumnName("MATERIA")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(50);
+    }
+}
