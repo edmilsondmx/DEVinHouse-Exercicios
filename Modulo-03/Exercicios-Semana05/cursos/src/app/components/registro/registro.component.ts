60ee120217458856dfe9cac599aadf6ca8cc0478
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-15 15:48:39 -0300

    [M3S07] EX 5 - FormGroup e FormControl

--- Modulo-03/Exercicios-Semana05/cursos/src/app/components/registro/registro.component.ts
+++ Modulo-03/Exercicios-Semana05/cursos/src/app/components/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  formRegister = this.formBuilder.group({
+    nome: this.formBuilder.control(''),
+    cargaHoraria: this.formBuilder.control(''),
+    escola: this.formBuilder.control('SENAI/SC - Serviço Nacional de Aprendizagem Industrial'),
+    professor: this.formBuilder.control(''),
+    materia: this.formBuilder.control(''),
+  })
+
+  showForm(){
+    console.log(this.formRegister)
   }
 
 }
