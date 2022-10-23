19c77ea013508c30968a6579de98fd256efee5b2
Edmilson Gomes <edmilsondmx@gmail.com>
2022-10-22 21:11:36 -0300

    [M3S08] EX 4 - Integridade ControleEntradasSaidas

--- Modulo-03/Exercicios-Semana08/Exercicios_Cypress/cypress/e2e/TesteEntradaSaida/EntradaSaidaEx1.cy.js
+++ Modulo-03/Exercicios-Semana08/Exercicios_Cypress/cypress/e2e/TesteEntradaSaida/EntradaSaidaEx1.cy.js
@@ -5,8 +5,25 @@ context('TestesEntradaSaidaEx1', () => {
     });
 
     afterEach(() => {
-        cy.get('input').contains('Enviar').click()
         cy.get('input').contains('Limpar').click()
+
+        for (let i = 1; i <= 7; i++) {
+            let txt = `#txtN${i}`
+            let vlr = `#txtV${i}`
+
+            cy.get('#form1')
+            .each(($el) => {
+
+                cy.get($el).find(txt)
+                .invoke('text').then(val => {
+                    expect(val).to.equal('')
+                })
+                cy.get($el).find(vlr)
+                .invoke('text').then(val => {
+                    expect(val).to.equal('')
+                })
+            })
+        }
         
     });
 
@@ -24,6 +41,65 @@ context('TestesEntradaSaidaEx1', () => {
         cy.get('#txtN4').type('Refrigerante')
         cy.get('#txtV4').type(6)
 
+        cy.get('input').contains('Enviar').click()
+
+        cy.get('table tbody tr').should('have.length', 5)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Boneca')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Carrinho')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Doces')
+                })
+
+            if(index == 4)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Refrigerante')
+                })
+
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('4')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('1')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('5')
+                })
+
+            if(index == 4)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('6')
+                })
+
+        })
     });
 
     it('testeImgEx1_entrada2', () => {
@@ -36,6 +112,52 @@ context('TestesEntradaSaidaEx1', () => {
 
         cy.get('#txtN3').type('Doces')
         cy.get('#txtV3').type(8)
+
+        cy.get('input').contains('Enviar').click()
+        
+        cy.get('table tbody tr').should('have.length', 4)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Boneca')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Bolo')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Doces')
+                })
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('78')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('3')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('8')
+                })
+        })
     });
 
     it('testeImgEx1_entrada3', () => {
@@ -52,8 +174,46 @@ context('TestesEntradaSaidaEx1', () => {
         cy.get('#txtN4').type('Bolo Seco')
         cy.get('#txtV4').type(1)
 
-        cy.get('#txtN4').type('Gelatina')
-        cy.get('#txtV4').type(2)
+        cy.get('#txtN5').type('Gelatina')
+        cy.get('#txtV5').type(2)
+
+        cy.get('input').contains('Enviar').click()
+        
+        cy.get('table tbody tr').should('have.length', 6)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Café')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Coxinha')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Pavê')
+                })
+
+            if(index == 4)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Bolo Seco')
+                })
+
+            if(index == 5)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Gelatina')
+                })
+
+        })
 
     });
 });
--- Modulo-03/Exercicios-Semana08/Exercicios_Cypress/cypress/e2e/TesteEntradaSaida/EntradaSaidaEx2.cy.js
+++ Modulo-03/Exercicios-Semana08/Exercicios_Cypress/cypress/e2e/TesteEntradaSaida/EntradaSaidaEx2.cy.js
@@ -5,8 +5,25 @@ context('TestesEntradaSaidaEx2', () => {
     });
 
     afterEach(() => {
-        cy.get('input').contains('Enviar').click()
         cy.get('input').contains('Limpar').click()
+
+        for (let i = 1; i <= 7; i++) {
+            let txt = `#txtN${i}`
+            let vlr = `#txtV${i}`
+
+            cy.get('#form1')
+            .each(($el) => {
+
+                cy.get($el).find(txt)
+                .invoke('text').then(val => {
+                    expect(val).to.equal('')
+                })
+                cy.get($el).find(vlr)
+                .invoke('text').then(val => {
+                    expect(val).to.equal('')
+                })
+            })
+        }
         
     });
 
@@ -18,12 +35,68 @@ context('TestesEntradaSaidaEx2', () => {
         cy.get('#txtN2').type('FolhaA4')
         cy.get('#txtV2').type(530)
 
+        cy.get('input').contains('Enviar').click()
+
+        cy.get('table tbody tr').should('have.length', 3)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Caneta')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('FolhaA4')
+                })
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('234')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('530')
+                })
+        })
+
     });
 
     it('testeImgEx2_entrada2', () => {
 
         cy.get('#txtN1').type('Lápis')
         cy.get('#txtV1').type(4320)
+        
+        cy.get('input').contains('Enviar').click()
+
+        cy.get('table tbody tr').should('have.length', 2)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Lápis')
+                })
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('4320')
+                })
+        })
     });
 
     it('testeImgEx2_entrada3', () => {
@@ -36,6 +109,52 @@ context('TestesEntradaSaidaEx2', () => {
 
         cy.get('#txtN3').type('Livros')
         cy.get('#txtV3').type(1200)
+        
+        cy.get('input').contains('Enviar').click()
+
+        cy.get('table tbody tr').should('have.length', 4)
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Bolsas')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Cadernos')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pNome')
+                .invoke('text').then(nome => {
+                    expect(nome).to.equal('Livros')
+                })
+        })
+
+        cy.get('tbody tr')
+        .each(($el, index) => {
+            if(index == 1)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('135')
+                })
+                
+            if(index == 2)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('1200')
+                })
+
+            if(index == 3)
+                cy.get($el).find('#pValor')
+                .invoke('text').then(valor => {
+                    expect(valor).to.equal('1200')
+                })
+        })
 
     });
 });
