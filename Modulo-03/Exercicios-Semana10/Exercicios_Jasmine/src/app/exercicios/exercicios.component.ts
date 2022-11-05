3c280da75ac45654b521708267f0a78c05beb0b1
Edmilson Gomes <edmilsondmx@gmail.com>
2022-11-05 16:52:31 -0300

    criacao projeto angular

---
+++ Modulo-03/Exercicios-Semana10/Exercicios_Jasmine/src/app/exercicios/exercicios.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exercicios',
+  templateUrl: './exercicios.component.html',
+  styleUrls: ['./exercicios.component.scss']
+})
+export class ExerciciosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  returnAluno(value:string):string{
+    let aluno = value;
+    return aluno;
+  }
+
+}
